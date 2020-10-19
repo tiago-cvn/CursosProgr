5bdcfc943b2fd020d59fdb0e629481dcb7809193
Tiago Alves <tiago.cvn@gmail.com>
2020-10-19 19:56:51 -0300

    Tiago
    Entrada de Dados

---
+++ Sistema/frm_consultaProdutos.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Sistema
+{
+    partial class frm_consultaProdutos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.produtosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.produtosDataGridView = new System.Windows.Forms.DataGridView();
+            this.btn_Consulta = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CB_Categoria = new System.Windows.Forms.ComboBox();
+            this.db_sistemaDataSet = new Sistema.db_sistemaDataSet();
+            this.tb_categoriaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tb_categoriaTableAdapter = new Sistema.db_sistemaDataSetTableAdapters.tb_categoriaTableAdapter();
+            this.tableAdapterManager = new Sistema.db_sistemaDataSetTableAdapters.TableAdapterManager();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.produtosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.produtosDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_sistemaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_categoriaBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // produtosBindingSource
+            // 
+            this.produtosBindingSource.DataSource = typeof(sistema.DAL.produtos);
+            // 
+            // produtosDataGridView
+            // 
+            this.produtosDataGridView.AllowUserToAddRows = false;
+            this.produtosDataGridView.AllowUserToDeleteRows = false;
+            this.produtosDataGridView.AutoGenerateColumns = false;
+            this.produtosDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.produtosDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.produtosDataGridView.DataSource = this.produtosBindingSource;
+            this.produtosDataGridView.Location = new System.Drawing.Point(2, 31);
+            this.produtosDataGridView.Name = "produtosDataGridView";
+            this.produtosDataGridView.ReadOnly = true;
+            this.produtosDataGridView.Size = new System.Drawing.Size(541, 220);
+            this.produtosDataGridView.TabIndex = 1;
+            // 
+            // btn_Consulta
+            // 
+            this.btn_Consulta.Location = new System.Drawing.Point(331, 2);
+            this.btn_Consulta.Name = "btn_Consulta";
+            this.btn_Consulta.Size = new System.Drawing.Size(75, 23);
+            this.btn_Consulta.TabIndex = 2;
+            this.btn_Consulta.Text = "Consulta";
+            this.btn_Consulta.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pesquisar";
+            // 
+            // CB_Categoria
+            // 
+            this.CB_Categoria.DataSource = this.tb_categoriaBindingSource;
+            this.CB_Categoria.DisplayMember = "desc_categoria";
+            this.CB_Categoria.FormattingEnabled = true;
+            this.CB_Categoria.Location = new System.Drawing.Point(71, 4);
+            this.CB_Categoria.Name = "CB_Categoria";
+            this.CB_Categoria.Size = new System.Drawing.Size(219, 21);
+            this.CB_Categoria.TabIndex = 4;
+            this.CB_Categoria.ValueMember = "id_categoria";
+            // 
+            // db_sistemaDataSet
+            // 
+            this.db_sistemaDataSet.DataSetName = "db_sistemaDataSet";
+            this.db_sistemaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tb_categoriaBindingSource
+            // 
+            this.tb_categoriaBindingSource.DataMember = "tb_categoria";
+            this.tb_categoriaBindingSource.DataSource = this.db_sistemaDataSet;
+            // 
+            // tb_categoriaTableAdapter
+            // 
+            this.tb_categoriaTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.tb_categoriaTableAdapter = this.tb_categoriaTableAdapter;
+            this.tableAdapterManager.UpdateOrder = Sistema.db_sistemaDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "id_produto";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Codigo";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "desc_produto";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Nome Produto";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 250;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "valor";
+            dataGridViewCellStyle1.Format = "C2";
+            dataGridViewCellStyle1.NullValue = null;
+            this.dataGridViewTextBoxColumn3.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewTextBoxColumn3.HeaderText = "Valor";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            this.dataGridViewTextBoxColumn3.Width = 150;
+            // 
+            // frm_consultaProdutos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(542, 254);
+            this.Controls.Add(this.CB_Categoria);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Consulta);
+            this.Controls.Add(this.produtosDataGridView);
+            this.Name = "frm_consultaProdutos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consultar Produtos por Categoria";
+            this.Load += new System.EventHandler(this.frm_consultaProdutos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.produtosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.produtosDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_sistemaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_categoriaBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource produtosBindingSource;
+        private System.Windows.Forms.DataGridView produtosDataGridView;
+        private System.Windows.Forms.Button btn_Consulta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CB_Categoria;
+        private db_sistemaDataSet db_sistemaDataSet;
+        private System.Windows.Forms.BindingSource tb_categoriaBindingSource;
+        private db_sistemaDataSetTableAdapters.tb_categoriaTableAdapter tb_categoriaTableAdapter;
+        private db_sistemaDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+    }
+}
